61e1f123ba76a5ca21a0669d471fab3452d0ff87
Andrew Pellett <anrope@gmail.com>
2011-11-24 19:10:23 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snflx::Application.config.secret_token = 'f4988aef3b4af70314ce29d5fc5f94c2b3fd5ff9cf803b18e646ae7c6a491017c08830cec5eebe53f0aa7ca2be588e2d1c51d0414d2925055649c0fbf4baee44'
